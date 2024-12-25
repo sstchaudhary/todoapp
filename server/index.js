c02491f14b7052ce86a4815f4cf74f52e3add225
sarvagya@github <sstchaudhary@gmail.com>
2024-12-25 16:21:03 +0530

    dockerised code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import  Connection  from './database/db.js';
 import route from './routes/route.js';
- import cors from 'cors';
-
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 const app=express();
  app.use(cors());
  app.use(express.json());
@@ -10,6 +11,6 @@ const app=express();
  app.use('/',route);
  Connection();
 const PORT=process.env.PORT||8000
- app.listen(PORT,'localhost',()=>{
+ app.listen(PORT,'0.0.0.0',()=>{
     console.log(`server running on ${PORT}`);
  })
