70fb20e1efccc1a1c26bf784cad06c42dcea2738
sarvagya@github <sstchaudhary@gmail.com>
2024-12-26 02:10:17 +0530

    env in react

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { ADDNEW_TODO, GETALL_TODO,TOGGLE_TODO ,UPDATE_TODO,DELETE_TODO,TOGGLE_TABS} from './type';
-const API_URL = 'http://localhost:8000'
+const API_URL=process.env.REACT_APP_API_URL||'http://localhost:8000'
 export const addNewTodo = (data) => async (dispatch) => {
     try {
         const res = await axios.post(`${API_URL}/todos`, { data });
